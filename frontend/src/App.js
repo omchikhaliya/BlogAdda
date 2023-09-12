2685c61f7c5f845e34b814db3dc780cbad7c6d4c
{omchikhaliya} <{omchikhaliya121@gmail.com}>
2023-09-12 21:50:41 +0530

    login-signup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import RegistrationForm from './components/RegistrationForm.js'
 import LoginForm from './components/LoginForm';
 
@@ -6,9 +7,22 @@ function App() {
   return (
     <div className="App">
       {/* <RegistrationForm /> */}
-      <h1 className='text-3xl font-bold underline'>Hello</h1>
-      <RegistrationForm />
+      <LoginForm /> 
     </div>
+
+    // <div className='App bg-gray-900 h-screen w-screen relative overflow-hidden flex flex-col justify-center items-center'>
+    //   <div className='h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 animate-pulse'></div>
+    //   <div className='h-35-r w-35-r bg-gradient-to-r from-red-400 via-pink-500 to-purple-500 rounded-full absolute top-96 -left-20 animate-pulse'></div>
+    //     <div className='container h-96 w-96 bg-white bg-opacity-10 relative z-2 rounded-2xl shadow-5xl border border-r-0 border-b-0 border-opacity-30 backdrop-filter backdrop-blur-sm'>
+    //       <form className='h-full flex flex-col justify-evenly items-center'>
+    //         <div className='font-poppins text-white text-2xl tracking-wider'>Login</div>
+    //         <input type='text' placeholder='username' className='input-text'></input>
+    //         <input type='password' placeholder='password' className='input-text'></input>
+    //         <input type="submit" className='font-poppins cursor-pointer px-5 py-1 rounded-full bg-white bg-opacity-50 hover:bg-opacity-80'/>
+    //       </form>
+    //     </div>
+    // </div>
+
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width : {
+        '40-r' : '40rem',
+        '35-r' : '30rem'
+      },
+      height : {
+        '40-r' : '40rem',
+        '35-r' : '30rem'
+      },
+      boxShadow: {
+        '5xl' : '20px 20px 50px rgba(0, 0, 0, 0.5)',
+      },
+      fontFamily: {
+        'poppins' : ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
-
